b9eb99b32e1d1b03957c7bd9a7b6d3a5d824a0f7
Fern Aragon <95953827+FERN1701@users.noreply.github.com>
2023-12-23 00:06:27 +0800

    first commit

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include "session.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $deleteId = $_POST['deleteId'];
--- display.php
+++ display.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include "session.php";
 
 // Get names from the session array
 $names = isset($_SESSION['names']) ? $_SESSION['names'] : array();
